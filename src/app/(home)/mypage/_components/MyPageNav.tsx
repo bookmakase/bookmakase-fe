98e04ade0dc3b834d1817df40ba0037b6931c138
GitHub Action <actions@github.com>
2025-07-19 15:26:13 +0900

    feat: 이미지 업로드 기능 추가

--- src/app/(home)/mypage/_components/MyPageNav.tsx
+++ src/app/(home)/mypage/_components/MyPageNav.tsx
@@ -1,9 +1,51 @@
+"use client";
+
+import { useUpdateProfileImage } from "@/hooks/mutation/useUpdateProfileImage";
+import { useMyIntro } from "@/hooks/query/useMyInfo";
+import { useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef, useState } from "react";
+import toast from "react-hot-toast";
 import { FaRegEdit } from "react-icons/fa";
 
 export default function MyPageNav() {
+  const { data } = useMyIntro();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const queryClient = useQueryClient();
+  const { mutate } = useUpdateProfileImage();
+
+  console.log(data);
+
+  const handleClickEdit = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      alert("이미지를 선택해주세요.");
+      return;
+    }
+
+    const url = URL.createObjectURL(file);
+    setPreviewUrl(url);
+
+    console.log("선택된 파일 : ", file);
+
+    mutate(file, {
+      onSuccess: () => {
+        toast.success("이미지 업로드 성공했습니다!");
+        queryClient.invalidateQueries({ queryKey: ["myIntro"] });
+      },
+      onError: () => {
+        alert("이미지 업로드 실패...");
+      },
+    });
+  };
+
   return (
     <div className="min-h-[calc(100vh-200px)] flex flex-col  items-center flex-1 border-r-2 gap-16 px-2 py-20">
       {/* 이미지 콘테이너 */}
@@ -11,21 +53,33 @@ export default function MyPageNav() {
         <div className="relative w-[190px] h-[190px] border-2 rounded-full overflow-hidden">
           <Image
             alt="프로필 이미지"
-            src={"/images/profile-image-default.jpg"}
+            src={
+              data.imageUrl || previewUrl || "/images/profile-image-default.jpg"
+            }
             fill
             priority
-            className="object-contain"
+            className="object-cover"
           />
         </div>
 
         <FaRegEdit
           size={42}
+          onClick={handleClickEdit}
           className="
           absolute bottom-6 right-2
           text-black drop-shadow-sm /* 배경 밝으면 테두리 주기 */
           z-10                     /* 이미지 위로 올라오게 */
           cursor-pointer
         "
+        />
+
+        {/* 숨겨진 파일 업로드 input */}
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className="hidden"
         />
       </div>
 
